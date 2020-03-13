e9d7d732feb76ab44d07965e77597dcf24ab8e10
sercangl <sercan1540@gmail.com>
2020-03-14 01:01:00 +0300

    JsonDecode Added

--- flutter_wheather_suggestions/lib/screens/loading_screen.dart
+++ flutter_wheather_suggestions/lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wheather_suggestions/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,12 +21,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Response response = await get(
+    http.Response response = await http.get(
         'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22');
-    print(response.body);
+    //print(response.body);
 
     if (response.statusCode == 200) {
-      print(response.body);
+      String data = response.body;
+
+      var lon = jsonDecode(data)['coord']['lon'];
+
+      print(lon);
     } else {
       print(response.statusCode);
     }
